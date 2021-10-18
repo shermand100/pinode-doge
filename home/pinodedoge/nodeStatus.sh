1c426c562ce35813e27b319fa810301622dac496
shermand100 <shermand100@gmail.com>
2021-10-19 00:39:01 +0100

    Improve %sync accuracy, de-clutter status readouts

--- home/pinodedoge/nodeStatus.sh
+++ home/pinodedoge/nodeStatus.sh
@@ -23,7 +23,7 @@ fi
 	if [ $BOOT_STATUS -eq 3 ]
 then	
 		#Node Status
-		NODE_STATUS="$(dogecoin/bin/dogecoin-cli getblockchaininfo | jq -Mr {"Network:.chain, Current_Block_height:.blocks, Sync_Progress_percent:((.verificationprogress)*10000|floor/100.0), Initial_block_download:.initialblockdownload, Approx_Size_on_Disk_GB:((.size_on_disk)/10000000|floor/100.0)"} | tr -d '{}"')" && echo "$NODE_STATUS"> /var/www/html/Node_Status.txt
+		NODE_STATUS="$(dogecoin/bin/dogecoin-cli getblockchaininfo | jq -Mr {"Network:.chain, Current_Block_height:.blocks, Sync_Progress_percent:((.verificationprogress)*100+0.0001|floor), Initial_block_download:.initialblockdownload, Approx_Size_on_Disk_GB:((.size_on_disk)/10000000|floor/100.0)"} | tr -d '{}"')" && echo "$NODE_STATUS"> /var/www/html/Node_Status.txt
 		#Time stamps added temporarily to better identify most recent stats, (not frozen/old data from browser cache).
 				date >> /var/www/html/Node_Status.txt
 fi
@@ -46,7 +46,7 @@ fi
 	if [ $BOOT_STATUS -eq 3 ]
 then	
 		#Connection Status
-	PRINT_CN="$(dogecoin/bin/dogecoin-cli getnetworkinfo | tr -d '{}"')" && echo "$PRINT_CN" > /var/www/html/print_cn.txt
+	PRINT_CN="$(dogecoin/bin/dogecoin-cli getnetworkinfo | jq -Mr {"This_Node_Running_Version:.subversion, Connected_Peer_Count:.connections, Available_Networks:.networks"} | tr -d '{}"')" && echo "$PRINT_CN" > /var/www/html/print_cn.txt
 	date >> /var/www/html/print_cn.txt
 fi	
 
@@ -68,7 +68,7 @@ fi
 	if [ $BOOT_STATUS -eq 3 ]
 then	
 		#Node Status
-		PRINT_POOL_STATS="$(dogecoin/bin/dogecoin-cli getmempoolinfo | tr -d '{}"')" && echo "$PRINT_POOL_STATS" > /var/www/html/TXPool_Status.txt
+		PRINT_POOL_STATS="$(dogecoin/bin/dogecoin-cli getmempoolinfo | jq -Mr {"TXs_in_Pool:.size, Pool_Size_Bytes:.bytes, Max_Pool_Size_MB:((.maxmempool)/10000000) "} | tr -d '{}"')" && echo "$PRINT_POOL_STATS" > /var/www/html/TXPool_Status.txt
 		date >> /var/www/html/TXPool_Status.txt
 fi	
 
@@ -91,7 +91,7 @@ fi
 	if [ $BOOT_STATUS -eq 3 ]
 then	
 		#Node Status
-		PRINT_TX_SHORT="$(dogecoin/bin/dogecoin-cli getrawmempool | tr -d '{}"')" && echo "$PRINT_TX_SHORT" > /var/www/html/TXPool-short_Status.txt
+		PRINT_TX_SHORT="$(dogecoin/bin/dogecoin-cli getrawmempool | tr -d '{}[]"')" && echo "$PRINT_TX_SHORT" > /var/www/html/TXPool-short_Status.txt
 		date >> /var/www/html/TXPool-short_Status.txt
 fi	
 
@@ -114,7 +114,7 @@ fi
 	if [ $BOOT_STATUS -eq 3 ]
 then	
 		#NetworkData
-		networkData="$(dogecoin/bin/dogecoin-cli getnettotals | tr -d '{}"')" && echo "$networkData" > /var/www/html/print_pl.txt
+		networkData="$(dogecoin/bin/dogecoin-cli getpeerinfo | jq -Mr '.[] | {Connected_Peer_id:.id, Connected_Peer_IP:.addr, RelayTXs:.relaytxes, Bytes_Sent:.bytessent, Bytes_Received:.bytesrecv, Inbound_Connection:.inbound, Connection_Time:((.conntime) | strftime("%Y-%m-%d %H:%M:%S LOCAL"))}' | tr -d '{}"')" && echo "$networkData" > /var/www/html/print_pl.txt
 		date >> /var/www/html/print_pl.txt
 fi	
 
