71de52ca987c56188c528e4d54df954b65f79809
shermand100 <shermand100@gmail.com>
2021-10-19 00:38:28 +0100

    Clarification of script purpose

--- home/pinodedoge/remove-autostart.sh
+++ home/pinodedoge/remove-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#On stopping node, revert "boot status" to mode 2. Prevents stopped node re-starting on auto-boot until manual command is sent.
+#On stopping node, revert "boot status" to mode 2. This allows the status script to return prints of --system idle--.
 	echo "#!/bin/sh
 BOOT_STATUS=2" > /home/pinodexmr/bootstatus.sh
