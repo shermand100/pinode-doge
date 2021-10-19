078663432a91963956909b4d0f9cacdbc69191ae
shermand100 <shermand100@gmail.com>
2021-10-19 01:07:31 +0100

    Fix broken path

--- HTML/nodeStop.php
+++ HTML/nodeStop.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
+  exec (". /home/pinodedoge/remove-autostart.sh");
   exec("sudo systemctl stop dogecoind-start.service");
   exec("sudo systemctl disable dogecoind-start.service");
   echo "Stop Command Sent for Private Node";
-  exec (". /home/pinodedoge/remove-autostart.sh");
  ?>
--- home/pinodedoge/remove-autostart.sh
+++ home/pinodedoge/remove-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #On stopping node, revert "boot status" to mode 2. This allows the status script to return prints of --system idle--.
 	echo "#!/bin/sh
-BOOT_STATUS=2" > /home/pinodexmr/bootstatus.sh
+BOOT_STATUS=2" > /home/pinodedoge/bootstatus.sh
