bae78f33b1f57234d2f53323c1a14d80493195f5
shermand100 <shermand100@gmail.com>
2022-08-31 14:30:31 +0100

    Dogecoin v1.14.6

--- downloadLatestDogecoin.sh
+++ downloadLatestDogecoin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Download
-wget https://github.com/dogecoin/dogecoin/releases/download/v1.14.5/dogecoin-1.14.5-arm-linux-gnueabihf.tar.gz
+wget https://github.com/dogecoin/dogecoin/releases/download/v1.14.6/dogecoin-1.14.6-arm-linux-gnueabihf.tar.gz
 #Unpack
-tar -zxvf dogecoin-1.14.5-arm-linux-gnueabihf.tar.gz
+tar -zxvf dogecoin-1.14.6-arm-linux-gnueabihf.tar.gz
 #For consistancy between versions, rename directory
-mv ~/dogecoin-1.14.5 ~/dogecoin
+mv ~/dogecoin-1.14.6 ~/dogecoin
 #Delete obsolete package
-rm dogecoin-1.14.5-arm-linux-gnueabihf.tar.gz
+rm dogecoin-1.14.6-arm-linux-gnueabihf.tar.gz
