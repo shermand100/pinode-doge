842a7bae0b2cb41ae3aeba36cb9e4c770cff8707
shermand100 <shermand100@gmail.com>
2024-12-02 10:39:34 +0000

    Update to Dogecoin v1.14.9

--- downloadLatestDogecoin.sh
+++ downloadLatestDogecoin.sh
@@ -8,28 +8,28 @@ if [ "$ARCH" = "armv7l" ]; then
   ##Get DOGECOIN
         echo "Download Dogecoin ARM package" >>debug.log
 #Download
-wget https://github.com/dogecoin/dogecoin/releases/download/v1.14.8/dogecoin-1.14.8-arm-linux-gnueabihf.tar.gz
+wget https://github.com/dogecoin/dogecoin/releases/download/v1.14.9/dogecoin-1.14.9-arm-linux-gnueabihf.tar.gz
 #Unpack
-tar -zxvf dogecoin-1.14.8-arm-linux-gnueabihf.tar.gz
+tar -zxvf dogecoin-1.14.9-arm-linux-gnueabihf.tar.gz
 
 #For consistancy between versions, remove previous and rename directory
 rm -rf dogecoin
-mv ~/dogecoin-1.14.8 ~/dogecoin
+mv ~/dogecoin-1.14.9 ~/dogecoin
 #Delete obsolete package
-rm dogecoin-1.14.8-arm-linux-gnueabihf.tar.gz
+rm dogecoin-1.14.9-arm-linux-gnueabihf.tar.gz
 else
   echo -e "\e[32mThis is aarch64 architecture.\e[0m"
   ##Get DOGECOIN
         echo "Download Dogecoin aarch64 package" >>debug.log
 #Download
-wget https://github.com/dogecoin/dogecoin/releases/download/v1.14.8/dogecoin-1.14.8-aarch64-linux-gnu.tar.gz
+wget https://github.com/dogecoin/dogecoin/releases/download/v1.14.9/dogecoin-1.14.9-aarch64-linux-gnu.tar.gz
 #Unpack
-tar -zxvf dogecoin-1.14.8-aarch64-linux-gnu.tar.gz
+tar -zxvf dogecoin-1.14.9-aarch64-linux-gnu.tar.gz
 #For consistancy between versions, rename directory
 rm -rf dogecoin
-mv ~/dogecoin-1.14.8 ~/dogecoin
+mv ~/dogecoin-1.14.9 ~/dogecoin
 #Delete obsolete package
-rm dogecoin-1.14.8-aarch64-linux-gnu.tar.gz
+rm dogecoin-1.14.9-aarch64-linux-gnu.tar.gz
 fi
 sleep 3
 
